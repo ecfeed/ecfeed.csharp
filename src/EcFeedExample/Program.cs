9a30dbceaf0a314690032bfd5e1c92bb1150b25d
Krzysztof Skorupski <k.skorupski@testify.no>
2019-03-22 15:49:01 +0100

    Initial commit

---
+++ src/EcFeedExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using EcFeed;
+
+namespace EcFeedExample
+{
+    class Example : Runner {
+
+        public override void ProcessResponseHandler(String line) {
+            Console.WriteLine(line);
+        }
+
+        static void Main(string[] args) {
+            string body = "{\"method\":\"public void com.example.test.LoanDecisionTest.generateCustomerData(java.lang.String,java.lang.String,com.example.test.Gender,int,java.lang.String,com.example.test.DocumentType)\",\"model\":\"0568-9381-1319-0545-5890\",\"userData\":\"{'dataSource':'genCartesian'}\"}";
+
+            Runner program = new Example();
+           
+            program.WebTask();
+            program.WebService(body);
+        }
+
+    }
+
+}

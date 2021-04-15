1356545918ceca7947103c5826d6f441650676ae
krzysztof skorupski <k.skorupski@testify.no>
2021-04-15 16:32:27 +0200

    update

--- exampleNUnit/NUnit.cs
+++ exampleNUnit/NUnit.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using NUnit.Framework;
 using EcFeed;
 
+using System.IO;
+using System;
+using System.Diagnostics;
+
 namespace exampleNUnit
 {
 
@@ -10,34 +14,35 @@ namespace exampleNUnit
     [TestFixture]
     public class UnitTest
     {
-        static public IEnumerable DataString = new TestProvider("7482-5194-2849-1943-2448").GenerateNWise("com.example.test.Demo.typeString");
-        static public IEnumerable DataInt = new TestProvider("7482-5194-2849-1943-2448").GenerateNWise("com.example.test.Demo.typeInt");
-        static public IEnumerable DataEnum = new TestProvider("7482-5194-2849-1943-2448").GenerateNWise("com.example.test.Demo.typeEnum", 1);
-        static public IEnumerable DataMix = new TestProvider("7482-5194-2849-1943-2448").GenerateNWise("com.example.test.Demo.typeMix");
+        static public IEnumerable DataString = new TestProvider("IMHL-K0DU-2U0I-J532-25J9").GenerateNWise("com.example.test.Playground.size_10x10");
+        // static public IEnumerable DataInt = new TestProvider("IMHL-K0DU-2U0I-J532-25J9").GenerateNWise("com.example.test.Demo.typeInt");
+        // static public IEnumerable DataEnum = new TestProvider("IMHL-K0DU-2U0I-J532-25J9").GenerateNWise("com.example.test.Demo.typeEnum", 1);
+        // static public IEnumerable DataMix = new TestProvider("IMHL-K0DU-2U0I-J532-25J9").GenerateNWise("com.example.test.Demo.typeMix");
 
         [TestCaseSource("DataString")]
-        public void TestString(string a0, string a1, string a2, string a3, string a4, string a5, string a6, string a7, string a8, string a9, string a10)
+        public void TestString(string a, string b, string c, string d, string e, string f, string g, string h, string i, string j, Feedback ecfeed)
         {
+            NUnit.Framework.TestContext.Progress.WriteLine(ecfeed.TestSessionId);
             // Assert.That(false, Is.True);
         }
 
-        [TestCaseSource("DataInt")]
-        public void TestInt(int a0, int a1)
-        {
-            // Assert.That(false, Is.True);
-        }
+        // [TestCaseSource("DataInt")]
+        // public void TestInt(int a0, int a1)
+        // {
+        //     // Assert.That(false, Is.True);
+        // }
 
-        [TestCaseSource("DataMix")]
-        public void TestMix(byte a0, short a1, int a3, long a4, float a5, double a6, bool a7)
-        {
-            // Assert.That(false, Is.True);
-        }
+        // [TestCaseSource("DataMix")]
+        // public void TestMix(byte a0, short a1, int a3, long a4, float a5, double a6, bool a7)
+        // {
+        //     // Assert.That(false, Is.True);
+        // }
 
-        [TestCaseSource("DataEnum")]
-        public void TestEnum(CSharpEnum a0)
-        {
-            // Assert.That(false, Is.True);
-        }
+        // [TestCaseSource("DataEnum")]
+        // public void TestEnum(CSharpEnum a0)
+        // {
+        //     // Assert.That(false, Is.True);
+        // }
     }
 
 }

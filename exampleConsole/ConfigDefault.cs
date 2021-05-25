4a1fa4d5c4f1c915df2734093daea242bed3f74f
krzysztof skorupski <k.skorupski@testify.no>
2021-05-25 12:00:12 +0200

    prod update

--- exampleConsole/ConfigDefault.cs
+++ exampleConsole/ConfigDefault.cs
@@ -5,7 +5,7 @@ namespace Testify.EcFeed.Example
     public class ConfigDefault
     {
 
-        static internal TestProvider TestProvider = new TestProvider("LRXC-015K-GJB0-2A9F-CGA2");
+        static internal TestProvider TestProvider = new TestProvider("IMHL-K0DU-2U0I-J532-25J9");
         static internal string F_10x10 = "com.example.test.Playground.size_10x10";
         static internal string F_100x2 = "com.example.test.Playground.size_100x2";
         static internal string F_TEST = "QuickStart.test";
--- exampleNUnit/ConfigDefault.cs
+++ exampleNUnit/ConfigDefault.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
 using EcFeed;
 
 namespace exampleNUnit
@@ -9,7 +5,7 @@ namespace exampleNUnit
     public class ConfigDefault
     {
 
-        internal static TestProvider TestProvider = new TestProvider("LRXC-015K-GJB0-2A9F-CGA2");
+        internal static TestProvider TestProvider = new TestProvider("IMHL-K0DU-2U0I-J532-25J9");
         internal static string F_10x10 = "com.example.test.Playground.size_10x10";
         internal static string F_100x2 = "com.example.test.Playground.size_100x2";
         internal static string F_TEST = "QuickStart.test";
